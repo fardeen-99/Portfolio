ccd56b0f00e06107ccb2256de6836a7caa182632
fardeen <fardeenkhan63574@gmail.com>
2026-02-09 23:38:27 +0530

    fix/ui

--- portfolio/src/component/Textpopup.jsx
+++ portfolio/src/component/Textpopup.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Textpopup = ({children,text}) => {
   return (
     <>
-    <div className='relative group icon hidden md:block'>
+    <div className='relative group  hidden md:block'>
 
     {children}
     <p className='absolute bottom-full left-1/2 -translate-x-1/2  mt-1  -translate-y-4  transition-all duration-200 font-bold opacity-0 scale-9 0 group-hover:opacity-100   group-hover:scale-100         group-hover:-translate-y-7 text-stone-200'>{text}</p>
